4dd40819f8806c2529c6f31e69df9f0428fcd0e1
Jeet Majumder <Jeetmajumder858@gmail.com>
2025-07-05 18:13:55 +0530

    image change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import logo from '../../public/Subidha-logo.jpg'
+import logo from '../../public/logo.jpg'
 export default function Home() {
   return (
     <div className="image">
